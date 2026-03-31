f6ba7ec432ed44188bacf5714534fc6e75b9cd3f
alexandre-rodrigues27 <rodriguesxandy82@gmail.com>
2026-03-31 10:49:53 -0300

    Create 10 - Time.java

---
+++ Aula 31-03/10 - Time.java
@@ -0,0 +1,37 @@
+package pkg;
+
+public class Time {
+	public String nome;
+    public int golsMarcados;
+    public int golsSofridos;
+
+    public Time(String nome, int golsMarcados, int golsSofridos) {
+        this.nome = nome;
+        this.golsMarcados = golsMarcados;
+        this.golsSofridos = golsSofridos;
+    }
+
+    public void marcarGol() {
+        golsMarcados++;
+    }
+
+    public void sofrerGol() {
+        golsSofridos++;
+    }
+
+    public int saldoGols() {
+        return golsMarcados - golsSofridos;
+    }
+    
+    public String resultadoPartida() {
+        if (saldoGols() > 0) {
+            return "Vitória";
+        } else if (saldoGols() == 0) {
+            return "Empate";
+        } else {
+            return "Derrota";
+        }
+    }
+}
+
+
