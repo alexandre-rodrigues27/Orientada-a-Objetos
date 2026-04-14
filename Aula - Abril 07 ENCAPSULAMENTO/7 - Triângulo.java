86ee94d1ede4ecf442a8125b66a522aaa7614ab6
alexandre-rodrigues27 <rodriguesxandy82@gmail.com>
2026-04-14 09:21:10 -0300

    Create 7 - Triângulo.java

---
+++ Aula - Abril 07 ENCAPSULAMENTO/7 - Triângulo.java
@@ -0,0 +1,42 @@
+package pkg3;
+
+public class Triangulo {
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+
+    public Triangulo() {
+    }
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public double getLadoC() {
+        return ladoC;
+    }
+
+    public void setLadoC(double ladoC) {
+        this.ladoC = ladoC;
+    }
+
+    public String verificaEquilatero() {
+        if (ladoA == ladoB && ladoB == ladoC) {
+            return "O triângulo é equilátero.";
+        } else {
+            return "O triângulo NÃO é equilátero.";
+        }
+    }
+}
