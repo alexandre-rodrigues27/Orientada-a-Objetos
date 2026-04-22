60b4e14749da6dfb5b51c73a936cd9170cdcb3f5
alexandre-rodrigues27 <rodriguesxandy82@gmail.com>
2026-04-22 06:40:16 -0300

    Create 10-Tarefa.java

---
+++ Exercícios 14-04/10-Tarefa.java
@@ -0,0 +1,16 @@
+class Tarefa {
+    String descricao;
+    boolean concluida = false;
+
+    // Método para concluir a tarefa
+    void concluirTarefa() {
+        concluida = true;
+    }
+
+    // Método para exibir a tarefa
+    void exibirTarefa() {
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Status: " + (concluida ? "Concluída" : "Pendente"));
+        System.out.println("----------------------");
+    }
+}
