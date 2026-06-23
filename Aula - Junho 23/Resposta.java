e7a1e61e50589bd5ff300b666c1f5f59fd20630a
alexandre-rodrigues27 <rodriguesxandy82@gmail.com>
2026-06-23 09:30:35 -0300

    Create Resposta.java

---
+++ Aula - Junho 23/Resposta.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Resposta {
+	private float resultado;
+ 
+	public Resposta(float resultado) {
+		super();
+		this.resultado = resultado;
+	}
+ 
+	public float getResultado() {
+		return resultado;
+	}
+ 
+	public void setResultado(float resultado) {
+		this.resultado = resultado;
+	}
+	
+}
